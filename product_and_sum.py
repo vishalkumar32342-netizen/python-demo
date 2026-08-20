56fa635a45f8d5c0dfadded33c8e2fc1c3bc09df
vishal raj <vishalkumar32342>
2026-08-20 21:43:11 +0530

    Added product and sum program

---
+++ product_and_sum.py
@@ -0,0 +1,7 @@
+#for product of 2 numbers
+a,b=[int(x) for x in input("Enter 2 numbers:").split()]
+print("product is :",a*b)
+
+#sum of 3 float numbers
+a,b,c=[float(x) for x in input("Enter 3 float numbers:").split(',')]
+print("The sum is :",a+b+c)
