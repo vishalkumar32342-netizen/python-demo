4477e1247b84bc1ae7f30d1b4460939284a5737c
vishal raj <vishalkumar32342>
2026-08-20 21:04:55 +0530

    Initial commit

---
+++ string.py
@@ -0,0 +1,8 @@
+s=input("Enter some string:")
+i=1
+for x in s:
+    print("the character present at positive index : {} and at neegative index :{} is {}" .format (i,i-len(s),x))
+    i=i+1
+
+s="123456789"
+print(s[2:-5:-4])
