82116764e96ed2c1a7e2d2e81be6c37ed86d7b66
vishal raj <vishalkumar32342>
2026-08-23 00:13:12 +0530

    Add conditional statements program

---
+++ conditional_statements.py
@@ -0,0 +1,26 @@
+#if condition
+name=input("Enter name :")
+if name=="vishal":
+    print("hello vishal")
+
+print("how are you!!")
+
+#if-else condition
+name=input("enter name :")
+if name=="vishal":
+    print("hello vishal how are you:")
+else:
+    print("hello guest how are you:")
+
+#if-elif-else
+name=input("Enter your name:")
+if name=="vishal":
+    print("hello vishal :")
+elif name=="ankit":
+    print("hello ankit :")
+elif name=="sagar":
+    print("hello sagar :")
+elif name=="ritu":
+    print("hello ritu :")
+else:
+    print("other name are not recommended,sorry:")
